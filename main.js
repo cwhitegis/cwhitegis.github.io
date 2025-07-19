abdadcf14275d4aea8706df2e3aedf87495d52d2
cwhitegis <cwhitegis@gmail.com>
2025-07-19 08:44:54 -1000

    added renderer

--- main.js
+++ main.js
@@ -5,38 +5,28 @@ require(["esri/Map",
          "esri/widgets/Search",
          "esri/views/SceneView"
         ], (Map,FeatureLayer,PopupTemplate,esriConfig,Search,SceneView) => {
- const opsBuildings = new FeatureLayer({
-   portalItem:{
-     id:'2be4ad6652c649cbaea2be211555b1cf'
-   }
- })
- const footprintLayer = new FeatureLayer({
-   url:"https://services.arcgis.com/tNJpAOha4mODLkXz/arcgis/rest/services/Structures_Facilities/FeatureServer/0",
-     renderer: {
-      type: 'simple',
-      symbol:{
-        type: "polygon-3d",  // autocasts as new SimpleFillSymbol()
-        symbolLayers:[{
-          type: "extrude",
-          material:{
-            color:"blue"
+
+ const footRenderer = {
+  type: 'simple',
+  symbol:{
+    type: "polygon-3d", 
+    symbolLayers:[{
+      type: "extrude",
+      material:{
+        color:"blue"
           },
-          edges:{
-            type:'solid',
-            color: [50, 50, 50, .7],
-            size: 1
+        edges:{
+          type:'solid',
+          color: [50, 50, 50, .7],
+          size: 1
           },
           size: 10
         }]
      }
-    }
- });
-  
- const parcelLayer = new FeatureLayer({
-    url: 'https://services.arcgis.com/tNJpAOha4mODLkXz/arcgis/rest/services/Cadastral_2020/FeatureServer/1',
-    opacity: 1,
-    renderer: {
-      type: 'simple',
+ };
+
+ const parcelRenderer = {
+   type: 'simple',
       symbol:{
         type: "simple-fill",  // autocasts as new SimpleFillSymbol()
         color: [ 51,51, 204, 0.9 ],
@@ -46,13 +36,22 @@ require(["esri/Map",
           width: 1
       }
      }
-    }
+ }
+
+ const footprintLayer = new FeatureLayer({
+  url:"https://services.arcgis.com/tNJpAOha4mODLkXz/arcgis/rest/services/Structures_Facilities/FeatureServer/0",
+  renderer:footRenderer
+ });
+  
+ const parcelLayer = new FeatureLayer({
+    url: 'https://services.arcgis.com/tNJpAOha4mODLkXz/arcgis/rest/services/Cadastral_2020/FeatureServer/1',
+    renderer: parcelRenderer
   });
   
  var map = new Map({
    basemap: "topo",
    ground: "world-elevation",
-   layers: [parcelLayer, footprintLayer,opsBuildings]
+   layers: [parcelLayer, footprintLayer]
  });
   
  var searchWidget = new Search({
@@ -62,12 +61,13 @@ require(["esri/Map",
  var view = new SceneView({
    container: "viewDiv",
    map: map,
+   center: [-50,30]
  });
   
   view.when(function(){
     view.goTo({
       center: [-157.8581,21.3099],
-      zoom: 12,
+      zoom: 15,
       heading: 0,
       tilt: 45
     })
